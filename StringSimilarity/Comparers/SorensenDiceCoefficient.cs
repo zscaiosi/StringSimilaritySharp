b3013a14ddc58024b6548a220c15c833071501be
zscaiosi <zscaio.si@gmail.com>
2020-01-08 10:11:12 -0300

    WIP;

--- StringSimilarity/Comparers/SorensenDiceCoefficient.cs
+++ StringSimilarity/Comparers/SorensenDiceCoefficient.cs
@@ -6,6 +6,7 @@ namespace StringSimilarity.Comparers
 {
     public class SorensenDiceCoefficient : IStringComparer
     {
+        private char[] FirstDigram {get;set;}
         public decimal CompareTwoStrings(string a, string b)
         {
             throw new NotImplementedException();
@@ -16,6 +17,7 @@ namespace StringSimilarity.Comparers
         }
         private decimal Compare()
         {
+
             return 0.0M;
         }
     }
