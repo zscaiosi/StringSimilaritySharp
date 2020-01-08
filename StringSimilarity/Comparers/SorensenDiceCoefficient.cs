818c4382b0a4e796dc3175af699a09036b16f2ad
zscaiosi <zscaio.si@gmail.com>
2020-01-08 12:32:21 -0300

    feature: SorensenDice implementation - not tested;

--- StringSimilarity/Comparers/SorensenDiceCoefficient.cs
+++ StringSimilarity/Comparers/SorensenDiceCoefficient.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using StringSimilarity.Enums;
 using StringSimilarity.Interfaces;
 
@@ -6,19 +8,82 @@ namespace StringSimilarity.Comparers
 {
     public class SorensenDiceCoefficient : IStringComparer
     {
-        private char[] FirstDigram {get;set;}
+        /// <summary>
+        /// Compares two strings
+        /// </summary>
+        /// <param name="a">Base string</param>
+        /// <param name="b">Comparing string</param>
+        /// <returns>The similarity coefficient</returns>
         public decimal CompareTwoStrings(string a, string b)
         {
-            throw new NotImplementedException();
+            if (!AreStringsValid(a, b))
+                return 0.0M;
+            
+            a = Regex.Replace(a, @"/\s+/g", "");
+            b = Regex.Replace(b, @"/\s+/g", "");
+            // Builds up bigrams
+            var firstBigrams = new Dictionary<string, int>();
+            var secondBigrams = new Dictionary<string, int>();
+            var intersectionSize = 0;
+
+            for (var i = 0; i < a.Length - 1; i++)
+            {
+                var bigram = a.Substring(i, i + 2);
+                var count = firstBigrams.ContainsKey(bigram) ?
+                    firstBigrams[bigram] + 1
+                :
+                    1;
+                // Updates value
+                firstBigrams[bigram] = count;
+            }
+
+            for (var j = 0; j < b.Length - 1; j++)
+            {
+                var bigram = b.Substring(j, j + 2);
+                var count = firstBigrams.ContainsKey(bigram) ?
+                    firstBigrams[bigram]
+                :
+                    0;
+
+                if (count > 0)
+                {
+                    firstBigrams[bigram] = count - 1;
+                    intersectionSize += 1;
+                }
+            }
+
+            return (decimal)(2.0 * intersectionSize) / (a.Length + b.Length);
         }
+        /// <summary>
+        /// Checks if strings are valid
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
         public bool AreStringsValid(params string[] p)
         {
-            throw new NotImplementedException();
-        }
-        private decimal Compare()
-        {
+            var response = true;
+            
+            if ( p.Length % 2 != 0 )
+                response = false;
 
-            return 0.0M;
+            foreach (var _ in p)
+            {
+                if (string.IsNullOrEmpty(_))
+                    response = false;
+                if ( _.Length < 2 )
+                    response = false;
+            }
+
+            return response;
+        }
+        /// <summary>
+        /// Finds best match in a collection of strings
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <returns></returns>
+        private string FindBestMatch(params string[] inputs)
+        {
+            return default(string);
         }
     }
 }
--- StringSimilarity/Matcher.cs
+++ StringSimilarity/Matcher.cs
@@ -1,9 +1,40 @@
 ﻿using System;
+using StringSimilarity.Comparers;
+using StringSimilarity.Enums;
 
 namespace StringSimilarity
 {
     public class Matcher
     {
-        
+        private string[] _words;
+        private MethodsEnum _method;
+        private SorensenDiceCoefficient _sdc;
+        public Matcher(MethodsEnum method, params string[] words)
+        {
+            _method = method;
+            _words = words;
+        }
+        /// <summary>
+        /// Gets the similarity's coefficient between two strings depending on the chosen method
+        /// </summary>
+        /// <returns></returns>
+        public decimal GetCoefficient()
+        {
+            switch (_method)
+            {
+                case MethodsEnum.SorensenDice:
+                    var sd = new SorensenDiceCoefficient();
+                    return sd.CompareTwoStrings(_words[0], _words[1]);
+                case MethodsEnum.Levenshtein:
+                    throw new NotImplementedException();
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public string GetBestMatch()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
